e58f7be1564beda9c5342e5475a63b784d4a0368
Miguel Antonio Peña Fraga <mapena.16@est.ucab.edu.ve>
2021-02-04 19:44:51 -0400

    Métodos de prueba para el apartado 2.a

--- ImplExample.java
+++ ImplExample.java
@@ -5,4 +5,34 @@ public class ImplExample implements RemoteInterface {
   public void printMsg() {  
     System.out.println("This is an example RMI program");  
   }  
+
+  //Apertura de cuenta - Apartado 2.a
+  public boolean checkDocumentID(String documentID) { 
+    System.out.println("Documento de identidad: " + documentID); 
+    return true;  
+  } 
+
+  public boolean registerClient(String documentID, String name, String username, String password) {
+    System.out.println("Documento de identidad: " + documentID);
+    System.out.println("Nombre: " + name); 
+    System.out.println("Username: " + username); 
+    System.out.println("Password: " + password);    
+    return true;  
+  } 
+
+  public boolean checkMaxAccounts(String documentID) {  
+    System.out.println("Documento de identidad: " + documentID);
+    return true;  
+  } 
+
+  public boolean verifyUser(String username, String password) {  
+    System.out.println("Username: " + username); 
+    System.out.println("Password: " + password); 
+    return true;  
+  } 
+
+  public String intialDeposit(float deposit) {  
+    System.out.println("Depósito inicial: " + deposit); 
+    return "4455-ALPO-555-44-22252";  
+  } 
 } 
--- RemoteInterface.java
+++ RemoteInterface.java
@@ -4,4 +4,11 @@ import java.rmi.RemoteException;
 // Creating Remote interface for our application 
 public interface RemoteInterface extends Remote {  
    void printMsg() throws RemoteException;  
+
+   //Apertura de cuenta - Apartado 2.a
+   boolean checkDocumentID(String documentID) throws RemoteException;
+   boolean registerClient(String documentID, String name, String username, String password) throws RemoteException;
+   boolean checkMaxAccounts(String documentID) throws RemoteException;
+   boolean verifyUser(String username, String password) throws RemoteException;
+   String intialDeposit(float deposit) throws RemoteException;
 } 
