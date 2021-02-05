f5be60971caa3180496db76dc5b0545bbe7f0ca5
Miguel Antonio Peña Fraga <mapena.16@est.ucab.edu.ve>
2021-02-05 12:07:26 -0400

    Clase de transacción

---
+++ Transaction.java
@@ -0,0 +1,51 @@
+import java.sql.Date;
+
+public class Transaction {
+  private int id;
+  private Double amount;
+  private Date date;
+  private String description;
+  private String type;
+  private Number source;
+  private Number destination;
+
+  public Transaction(int id, Double amount, Date date, String description, String type, Number source, Number destination){
+    this.id = id;
+    this.amount = amount;
+    this.date = date;
+    this.description = description;
+    this.type = type;
+    this.source = source;
+    this.destination = destination;
+  }
+
+  public int getTransactionID(){
+    return this.id;
+  }
+
+  public Double getTransactionAmount(){
+    return this.amount;
+  }
+
+  public Date getTransactionDate(){
+    return this.date;
+  }
+
+  public String getTransactionDescription(){
+    return this.description;
+  }
+
+  public String getTransactionType(){
+    return this.type;
+  }
+
+  public Number getTransactionSource(){
+    return this.source;
+  }
+
+  public Number getTransactionDestination(){
+    return this.destination;
+  }
+  
+}
+
